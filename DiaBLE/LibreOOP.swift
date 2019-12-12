115e54b66721b56759c7a8623dcf6d27c8b13a74
Guido <gui_dos@libero.it>
2019-12-12 20:52:53 +0100

    Extract from mainDelegate.swift Transmitter, Sensor and LibreOOP related classes and structs.

---
+++ DiaBLE/LibreOOP.swift
@@ -0,0 +1,88 @@
+import Foundation
+
+// https://github.com/bubbledevteam/xdripswift/blob/master/xdrip/Transmitter/CGMBluetoothTransmitter/Libre/Utilities/LibreRawGlucoseData.swift
+
+struct HistoricGlucose: Codable {
+    let dataQuality: Int
+    let id: Int
+    let value: Int
+}
+
+struct OOPHistoryData: Codable {
+    var alarm: String
+    var esaMinutesToWait: Int
+    var historicGlucose: [HistoricGlucose]
+    var isActionable: Bool
+    var lsaDetected: Bool
+    var realTimeGlucose: HistoricGlucose
+    var trendArrow: String
+
+    func glucoseData(date: Date) -> (GlucoseMeasurement, [GlucoseMeasurement]) {
+        let current = GlucoseMeasurement(rawGlucose: realTimeGlucose.value, date: date)
+        var array = [GlucoseMeasurement]()
+        let gap: TimeInterval = 60 * 15
+        var date = date
+        var history = historicGlucose
+        if history.first!.id < history.last!.id {
+            history = history.reversed()
+        }
+        for g in history {
+            date = date.addingTimeInterval(-gap)
+            if g.dataQuality != 0 { continue }
+            let glucose = GlucoseMeasurement(rawGlucose: g.value, minutesCounter: g.id, date: date)
+            array.append(glucose)
+        }
+        return (current, array)
+    }
+}
+
+struct OOPCalibrationResponse: Codable {
+    let errcode: Int
+    let parameters: CalibrationParameters
+    enum CodingKeys: String, CodingKey {
+        case errcode
+        case parameters = "slope"
+    }
+}
+
+// https://github.com/JohanDegraeve/xdripswift/blob/master/xdrip/Transmitter/CGMBluetoothTransmitter/Libre/Utilities/LibreOOPClient.swift
+// https://github.com/bubbledevteam/xdripswift/blob/master/xdrip/Transmitter/CGMBluetoothTransmitter/Libre/Utilities/LibreOOPClient.swift
+// https://github.com/bubbledevteam/xdripswift/commit/a1779402
+
+func postToLibreOOP(bytes: Data, patchUid: Data? = nil, patchInfo: Data? = nil, completion: @escaping (_ data: Data?, _ errorDescription: String?) -> Void) {
+    var site = "http://www.glucose.space/"
+    site += patchInfo == nil ? "calibrateSensor" : "libreoop2"
+    let token = "bubble-201907"
+    let date = Int64((Date().timeIntervalSince1970 * 1000.0).rounded())
+    var json = ["content": "\(bytes.hex)"]
+    if let patchInfo = patchInfo {
+        json["accesstoken"] = token
+        json["patchUid"] = patchUid!.hex
+        json["patchInfo"] = patchInfo.hex
+    } else {
+        json["token"] = token
+        json["timestamp"] = "\(date)"
+    }
+    let request = NSMutableURLRequest(url: URL(string: site)!)
+    request.httpMethod = "POST"
+    var allowedCharset = NSMutableCharacterSet.alphanumeric()
+    allowedCharset.addCharacters(in: "-._~")
+    let parameters = json.map { "\($0.0)=\($0.1.addingPercentEncoding(withAllowedCharacters: allowedCharset as CharacterSet)!)" }
+    request.httpBody = parameters.joined(separator: "&").data(using: .utf8)
+    request.setValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
+    URLSession.shared.dataTask(with: request as URLRequest) {
+        data, urlResponse, error in
+        var errorDescription: String? = nil
+        defer {
+            DispatchQueue.main.sync {
+                completion(data, errorDescription)
+            }
+        }
+        if let error = error {
+            errorDescription = error.localizedDescription
+            return
+        }
+    }.resume()
+}
+
+
---
+++ DiaBLE/Transmitter.swift
@@ -0,0 +1,126 @@
+import Foundation
+import CoreBluetooth
+
+class Transmitter {
+
+    static let deviceInfoServiceUUID = "180A"
+    static let modelCharacteristicUUID    = "2A24"
+    static let serialCharacteristicUUID   = "2A25"
+    static let firmwareCharacteristicUUID = "2A26"
+    static let hardwareCharacteristicUUID = "2A27"
+    static let softwareCharacteristicUUID = "2A28"
+    static let manufacturerCharacteristicUUID = "2A29"
+
+    static let batteryInfoServiceUUID = "180F"
+    static let batteryVoltageCharacteristicUUID = "2A19"
+
+    var peripheral: CBPeripheral?
+    var name: String { "Unnamed peripheral" }
+    class var dataServiceUUID: String { "" }
+    class var dataReadCharacteristicUUID: String { "" }
+    class var dataWriteCharacteristicUUID: String { "" }
+
+    var readCharacteristic: CBCharacteristic?
+    var writeCharacteristic: CBCharacteristic?
+
+    var firmware = ""
+    var buffer = Data()
+    var fram = Data()
+    var patchUid = Data()
+    var patchInfo = Data()
+
+    init(peripheral: CBPeripheral) {
+        self.peripheral = peripheral
+    }
+
+    func write(_ bytes: Array<UInt8>) {
+        peripheral?.writeValue(Data(bytes), for: writeCharacteristic!, type: .withoutResponse)
+    }
+}
+
+class Bubble: Transmitter {
+    override var name: String { "Bubble" }
+    override class var dataServiceUUID: String { "6E400001-B5A3-F393-E0A9-E50E24DCCA9E" }
+    override class var dataReadCharacteristicUUID: String { "6E400003-B5A3-F393-E0A9-E50E24DCCA9E" }
+    override class var dataWriteCharacteristicUUID: String { "6E400002-B5A3-F393-E0A9-E50E24DCCA9E" }
+
+    enum ResponseType: UInt8, CustomStringConvertible {
+        case dataInfo =     0x80
+        case dataPacket =   0x82
+        case noSensor =     0xBF
+        case serialNumber = 0xC0
+        case patchInfo =    0xC1
+
+        var description: String {
+            switch self {
+            case .dataInfo:
+                return "Data info received"
+            case .dataPacket:
+                return "Data packet received"
+            case .noSensor:
+                return "No sensor detected"
+            case .serialNumber:
+                return "Serial number received"
+            case .patchInfo:
+                return "Patch info received"
+            }
+        }
+    }
+}
+
+class Droplet: Transmitter {
+    override var name: String { "Droplet" }
+    override class var dataServiceUUID: String { "C97433F0-BE8F-4DC8-B6F0-5343E6100EB4" }
+    override class var dataReadCharacteristicUUID: String { "C97433F1-BE8F-4DC8-B6F0-5343E6100EB4" }
+    override class var dataWriteCharacteristicUUID: String { "C97433F2-BE8F-4DC8-B6F0-5343E6100EB4" }
+
+    enum LibreType: String, CustomStringConvertible {
+        case L1   = "10"
+        case L2   = "20"
+        case US14 = "30"
+        case Lpro = "40"
+
+        var description: String {
+            switch self {
+            case .L1:
+                return "Libre 1"
+            case .L2:
+                return "Libre 2"
+            case .US14:
+                return "Libre US 14d"
+            case .Lpro:
+                return "Libre Pro"
+            }
+        }
+    }
+}
+
+class Limitter: Droplet {
+}
+
+class MiaoMiao: Transmitter {
+    override var name: String { "MiaoMiao" }
+    override class var dataServiceUUID: String { "6E400001-B5A3-F393-E0A9-E50E24DCCA9E" }
+    override class var dataReadCharacteristicUUID: String { "6E400003-B5A3-F393-E0A9-E50E24DCCA9E" }
+    override class var dataWriteCharacteristicUUID: String { "6E400002-B5A3-F393-E0A9-E50E24DCCA9E" }
+
+    enum ResponseType: UInt8, CustomStringConvertible {
+        case dataPacket = 0x28
+        case newSensor =  0x32
+        case noSensor =   0x34
+        case frequencyChange = 0xD1
+
+        var description: String {
+            switch self {
+            case .dataPacket:
+                return "Data packet received"
+            case .newSensor:
+                return "New sensor detected"
+            case .noSensor:
+                return "No sensor detected"
+            case .frequencyChange:
+                return "Reading frequency change"
+            }
+        }
+    }
+}
