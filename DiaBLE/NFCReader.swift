ba6ed10368abd312458a079ade1f1212e255dcab
Guido <gui_dos@libero.it>
2019-12-19 13:49:50 +0100

    Print to the main LogView

--- DiaBLE/NFCReader.swift
+++ DiaBLE/NFCReader.swift
@@ -13,24 +13,23 @@ class NFCReader: NSObject, NFCTagReaderSessionDelegate {
     }
 
     public func tagReaderSessionDidBecomeActive(_ session: NFCTagReaderSession) {
-        print("NFC: Session Did Become Active")
+        main.log("NFC: Session Did Become Active")
     }
 
     public func tagReaderSession(_ session: NFCTagReaderSession, didInvalidateWithError error: Error) {
-        print("NFC: Session Did Invalidate With Error")
-        print(error.localizedDescription)
+        main.log("NFC: \(error.localizedDescription)")
         // session.invalidate();
     }
 
     public func tagReaderSession(_ session: NFCTagReaderSession, didDetect tags: [NFCTag]) {
-        print("NFC: Did Detect Tags")
+        main.log("NFC: Did Detect Tags")
         guard let tag = tags.first else { return }
 
         if case .iso15693(let iso15693Tag) = tag {
 
             session.connect(to: tag) { error in
                 if error != nil {
-                    print(error!.localizedDescription)
+                    self.main.log(error!.localizedDescription)
                     session.invalidate(errorMessage: "Connection failure")
                     return
                 }
@@ -38,26 +37,26 @@ class NFCReader: NSObject, NFCTagReaderSessionDelegate {
                 iso15693Tag.getSystemInfo(requestFlags: [.address, .highDataRate]) {  (dfsid: Int, afi: Int, blockSize: Int, memorySize: Int, icRef: Int, error: Error?) in
                     if error != nil {
                         session.invalidate(errorMessage: "getSystemInfo error: " + error!.localizedDescription)
-                        print("NFC: error while getSystemInfo: \(error!.localizedDescription)")
+                        self.main.log("NFC: error while getSystemInfo: \(error!.localizedDescription)")
                         return
                     }
                     let uidString = iso15693Tag.identifier.hex
                     session.alertMessage = "Tag UID : \(uidString)"
-                    print("IC Identifier = \(uidString)")
-                    print("IC ManufacturerCode = \(iso15693Tag.icManufacturerCode)")
-                    print(String(format: "IC Reference 0x%X", icRef))
-                    print(String(format: "Block Size %d", blockSize))
-                    print(String(format: "Memory Size %d blocks", memorySize))
+                    self.main.log("IC Identifier: \(uidString)")
+                    self.main.log("IC ManufacturerCode: \(iso15693Tag.icManufacturerCode)")
+                    self.main.log(String(format: "IC Reference: 0x%X", icRef))
+                    self.main.log(String(format: "Block Size: %d", blockSize))
+                    self.main.log(String(format: "Memory Size: %d blocks", memorySize))
                 }
 
                 iso15693Tag.readSingleBlock(requestFlags: [.highDataRate, .address], blockNumber: 0) { (data, error) in
                     if error != nil {
-                        print("Error while reading single block: \(error!.localizedDescription)")
+                        self.main.log("Error while reading single block: \(error!.localizedDescription)")
                         session.invalidate(errorMessage: "Error while reading single block: \(error!.localizedDescription)")
                         return
                     }
 
-                    print("NFC Block 0: \(data.hex)")
+                    self.main.log("NFC Block 0: \(data.hex)")
 
                     session.invalidate()
                 }
